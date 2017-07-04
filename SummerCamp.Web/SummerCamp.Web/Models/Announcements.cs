fd277486bcded1eba66aa186f8d08eb422768db5
MihaiToropu <toropumihai@gmail.com>
2017-07-04 13:02:28 +0300

    Announcements list + search

---
+++ SummerCamp.Web/SummerCamp.Web/Models/Announcements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SummerCamp.Web.Models
+{
+    public class Announcements
+    {
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Closed { get; set; }
+
+    }
+}
